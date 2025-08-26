5d1f1d7b3482c76369880fc1266a38d71decb884
jared <114547220+goldjared@users.noreply.github.com>
2025-08-26 14:28:02 -0400

    make c dir, c warm up file 1

---
+++ c-practice/userAgeInput.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * Create a program that asks the user for their age as input. If the age is greater than or equal to 18, 
+ * the program should output "You are an adult." Otherwise, it should output "You are a minor." 
+*/
+void getUserAge() {
+	int age;
+	printf("Enter your age: ");
+	scanf("%d", &age);
+//	int flag1 = age>=18;
+//	printf("flag1: %d", flag1);
+	printf(age>=18 ? "You are an adult.\n" : "You are a minor.\n");
+}
+
+int main() {
+	getUserAge();
+	return 0;
+}
+
+
