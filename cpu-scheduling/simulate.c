7bf5e1d259e8998312129a3b015c1b7a22af5327
Jared Gold <114547220+goldjared@users.noreply.github.com>
2025-10-02 22:07:22 -0400

    create fcfs sim, create printOrder fn

--- cpu-scheduling/simulate.c
+++ cpu-scheduling/simulate.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TIME_QUANTUM 4;
 
@@ -7,6 +8,76 @@ struct simulatedProc {
 	int arrivalTime;
 	int burstTime;
 };
+
+int compareArrival(const void * a, const void * b) {
+	const struct simulatedProc *pa = (const struct simulatedProc *)a;
+	const struct simulatedProc *pb = (const struct simulatedProc *)b;
+
+	if (pa->arrivalTime < pb->arrivalTime) return -1;
+	if (pa->arrivalTime > pb->arrivalTime) return 1;
+	return 0;
+}
+
+void printOrder(int execLog[], int size) {
+	printf("Order: ");
+	for(int i = 0; i < size; i++) {
+		printf("P%d", execLog[i]);
+		if(i + 1 != size) printf(" -> ");
+	}
+
+	printf("\n");
+}
+
+
+void FCFS(struct simulatedProc processList[], int size) {
+/*
+ *
+	** FCFS:
+* 	list input is exact order to print/execute on
+* 	create 2d array of [n][2], where each arr will store wait, turnaround time
+*	
+*	int time = 0
+*	double waitSum = 0;
+*	double turnaroundSum = 0;
+	init time to first arrival time
+* 	iterate to n (n is len proc list)
+* 		process_date[i][0] = time - arrival (set wait time)
+*
+* 		time += burst_time
+* 		process_date[i][1] = process_date[i][0] + burst_time 
+* 		waitSum += process_date[i][0]
+* 		turnAroundSum += process_date[i][1]
+*
+* 	print process list
+* 	print waitSum / n
+* 	print turnaroundSum / n
+*
+*
+	*/
+
+	// 2d array wait time, turnaround time, where i+1 = process id
+	int processData[size][2]; 
+	int time = processList[0].arrivalTime;
+	double waitSum = 0;
+	double turnAroundSum = 0;
+
+	int execLog[100];
+	int execCount = 0;
+	for(int i = 0; i < size; i++) {
+		processData[i][0] = time - processList[i].arrivalTime;
+		time += processList[i].burstTime;
+		processData[i][1] = processData[i][0] + processList[i].burstTime;
+		waitSum += processData[i][0];
+		turnAroundSum += processData[i][1];
+
+		execLog[execCount++] = processList[i].id;
+	}
+	printf("Simulating FCFS...\n");
+	printOrder(execLog, execCount);
+	printf("Avg Waiting Time: %.2f\n", waitSum / size);
+	printf("Avg Turnaround Time: %.2f\n", turnAroundSum / size);
+
+}
 
 int main(void) {
 
@@ -22,6 +93,7 @@ int main(void) {
 	}
 
 	// build list of processes
+	printf("Enter process details:\n");
 	struct simulatedProc procList[processCount];
 	for(int i = 0; i < processCount; i++) {
 		procList[i].id = i + 1;
@@ -54,7 +126,12 @@ int main(void) {
 		}
 		
 		printf("\n");
-	}
+	} // end for
+
+	// sort proc list based on arrival times
+	qsort(procList, processCount, sizeof(struct simulatedProc), compareArrival);
+	// init FCFS
+	FCFS(procList, processCount);
 
 
 	return 1;
